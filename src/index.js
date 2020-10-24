351d2ec83054528e225261b8a7721222c928e86e
VladSelikh <vladikh2405@gmail.com>
2020-10-24 08:44:39 +0300

    fix: correct function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 module.exports = function reverse (n) {
   let newStr = n.toString();
   let newArr = [];
-  for (let i = newStr.length - 1; i > 0; i--) {
+  for (let i = newStr.length - 1; i > -1; i--) {
     newArr.push(newStr[i]);
   }
-  newStr = newArr.toString();
-  return parseInt(newStr);
+  let newStrNum = "";
+  for (let i = 0; i < newArr.length; i++) {
+    newStrNum += newArr[i];
+  }
+  //return parseInt(newStr);
+  return parseInt(newStrNum);
 }
