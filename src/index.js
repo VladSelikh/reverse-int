8eafc9de986ea765d89f140f74aa658e2793b301
VladSelikh <vladikh2405@gmail.com>
2020-10-24 08:43:13 +0300

    feat: reverse all integers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function reverse (n) {
-  let newStr = reverse.toString();
+  let newStr = n.toString();
   let newArr = [];
   for (let i = newStr.length - 1; i > 0; i--) {
     newArr.push(newStr[i]);
   }
   newStr = newArr.toString();
-  return Number(newStr);
+  return parseInt(newStr);
 }
